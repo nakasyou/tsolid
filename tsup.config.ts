a9255f4d33464bcb1128e6b624cf88b1a3f36afd
nakasyou <syoutarou1103@outlook.jp>
2025-04-29 16:25:26 +0900

    feat: jsx preserve

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,9 +2,17 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/core/index.ts', 'src/babel/index.ts'],
-  splitting: false,
+  splitting: true,
   sourcemap: true,
   clean: true,
   dts: true,
-  format: ['esm']
+  format: ['esm'],
+  esbuildOptions: (opts) => {
+    opts.jsx = 'preserve'
+  },
+  outExtension(ctx) {
+    return {
+      js: `.jsx`
+    }
+  },
 })
